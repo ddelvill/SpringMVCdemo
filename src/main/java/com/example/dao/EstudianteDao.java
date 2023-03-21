2046f12a745caf41be2614b3b375379f8eaa310f
=ddelvill <dayanasdelvillar@gmail.com>
2023-03-21 10:36:35 +0100

    Anotción repository eliminada.

--- src/main/java/com/example/dao/EstudianteDao.java
+++ src/main/java/com/example/dao/EstudianteDao.java
@@ -1,11 +1,10 @@
 package com.example.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.entities.Estudiante;
 
-@Repository // Con esta anotación creamos bins automáticamente. 
+// Con esta anotación creamos bins automáticamente. 
 
 
 public interface EstudianteDao extends JpaRepository<Estudiante, Integer> {
