8e625aa86358dbd622cb3d5bef48424888cbb001
=ddelvill <dayanasdelvillar@gmail.com>
2023-03-21 09:56:49 +0100

    Lala2

--- src/main/java/com/example/SpringMvCdemoApplication.java
+++ src/main/java/com/example/SpringMvCdemoApplication.java
@@ -2,8 +2,6 @@ package com.example;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
